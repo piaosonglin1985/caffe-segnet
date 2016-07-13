561a2015a2efb22ae7cb769b69724e5280147a59
Songlin Piao <piaosonglin1985@hotmail.com>
2016-07-13 18:20:44 +0200

    1 adapted SegNet for compiling 2 tested for training 3 extract changes from softmax_loss_layer from SegNet to the latest caffe implementation

---
+++ include/caffe/layers/dense_image_data_layer.hpp
@@ -0,0 +1,50 @@
+/*
+ * denseimage_data_layer.hpp
+ *
+ *  Created on: Jul 12, 2016
+ *      Author: piao
+ */
+
+#ifndef CAFFE_DENSE_IMAGE_DATA_LAYER_HPP_
+#define CAFFE_DENSE_IMAGE_DATA_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/data_transformer.hpp"
+#include "caffe/internal_thread.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+#include "caffe/util/blocking_queue.hpp"
+#include "caffe/layers/base_data_layer.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+class DenseImageDataLayer : public BasePrefetchingDataLayer<Dtype> {
+ public:
+  explicit DenseImageDataLayer(const LayerParameter& param)
+      : BasePrefetchingDataLayer<Dtype>(param) {}
+  virtual ~DenseImageDataLayer();
+  virtual void DataLayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "DenseImageData"; }
+  virtual inline int ExactNumBottomBlobs() const { return 0; }
+  virtual inline int ExactNumTopBlobs() const { return 2; }
+
+ protected:
+  shared_ptr<Caffe::RNG> prefetch_rng_;
+  virtual void ShuffleImages();
+  virtual void load_batch(Batch<Dtype>* batch);
+
+  vector<std::pair<std::string, std::string> > lines_;
+  int lines_id_;
+  Blob<Dtype> transformed_label_;
+};
+
+}  // namespace caffe
+
+
+
+#endif /* CAFFE_DENSE_IMAGE_DATA_LAYER_HPP_ */
